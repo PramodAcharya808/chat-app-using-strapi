f23e9a95d60f16d1c891d2cee08bcc38aa1c73a8
Pramod_Acharya <pramodacharya808@gmail.com>
2024-07-06 21:35:19 +0530

    revamed the project

---
+++ real-time-chat-server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ee31fd56bab7ecdbdc26ba1f26ac284b'),
+    },
+  },
+});
